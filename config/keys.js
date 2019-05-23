bd1c7dfeedfbec0a2d0da39a72ba838188392121
MarcDuyukun <duyukun1204@163.com>
2019-05-23 17:13:07 +0800

    init serve01

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://test:test1234@ds141697.mlab.com:41697/jd-interface',
+  mongoURI: 'mongodb+srv://jd-test:1092145194@cluster0-dwcme.mongodb.net/test?retryWrites=true',
   secretOrKey: 'secret'
 };
